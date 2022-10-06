83bafa16bb6aaf5629311e6ccdec3d08a82b7211
stefanlight <steffmukhin@gmail.com>
2022-10-06 04:02:47 +0300

    Started to write a bot

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from .bot import DiscordBot
+
+instance: "DiscordBot"
--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,36 @@
-# there is a little problem. I want to sleep and I didn't do the core of the bot, please don't be angry :(
+from typing import Union
+
+from aiohttp import ClientSession
+from disnake.ext.commands import AutoShardedBot
+
+import src
+
+
+class DiscordBot(AutoShardedBot):
+    def __init__(self, bot_session: ClientSession, bot_config, **kwargs) -> None:
+        self.session = bot_session
+        self.config = bot_config
+        self.log = ...
+        self.ready_at_once: bool = False
+        self.guilds_on_startup: Union[None, int] = None
+        super().__init__(**kwargs)
+
+    async def __aenter__(self) -> src.instance:
+        self.load_all_extensions()
+        return super()
+
+    async def __aexit__(self) -> None:
+        await self.session.close()
+        self.log.info("Bye bye")
+
+    async def on_ready(self) -> None:
+        if not self.ready_at_once:
+            self.guilds_on_startup = len(self.guilds)
+            self.ready_at_once = True
+            self.log.info("Bot is ready to work")
+
+    async def on_connect(self) -> None:
+        self.log.info("Connected to Discord API")
+
+    def load_all_extensions(self) -> None:
+        pass
